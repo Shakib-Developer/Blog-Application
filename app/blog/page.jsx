d2285cf3e07dcc8b2c8bed93c23161a63c400fa2
Shakib <sarkeryasin614@gmail.com>
2023-08-23 23:05:18 +0600

    first commit

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -10,7 +10,7 @@ export default async function page() {
         <ul className="postul">
           {posts.map((post) => (
             <li className="my-5 postli" key={post.id}>
-              <Link className="postlink" href={`./single-blog/${post.id}`}>
+              <Link className="postlink" href={`/single-blog/${post.id}`}>
                 <img
                   src="https://roar.media/wp-content/uploads/2023/07/18.png"
                   alt="Blog Post"
@@ -25,3 +25,39 @@ export default async function page() {
     </div>
   );
 }
+
+// import getAllPosts from "@/lib/getAllPosts";
+// import Link from "next/link";
+
+// const page = async () => {
+//   const postBlog = await getAllPosts();
+//   return (
+//     <>
+//       <div className="container mx-auto my-2">
+//         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+//           {postBlog.length === 0 ? (
+//             <h2 className="text-red-500">Lodding......</h2>
+//           ) : (
+//             postBlog.map((posts, index) => (
+//               <Link
+//                 href={`/blog/${posts.id}`}
+//                 key={index.toString()}
+//                 className="card card-compact w-100 glass"
+//               >
+//                 <figure>
+//                   <img src={posts.img} alt="Post-image" />
+//                 </figure>
+//                 <div className="card-body">
+//                   <h2 className="card-title"> {posts.title} </h2>
+//                   <p> {posts.short} </p>
+//                 </div>
+//               </Link>
+//             ))
+//           )}
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default page;
--- app/single-blog/[id]/page.jsx
+++ app/single-blog/[id]/page.jsx
@@ -2,16 +2,23 @@ import getAllPosts from "@/lib/getAllPosts";
 import getSinglePost from "@/lib/getSinglePost";
 
 export default async function page({ params }) {
-  const { id } = params;
+  const id = params.id;
 
   const post = await getSinglePost(id);
 
   return (
     <div>
       <section>
-        <h1 className="font-bold text-lg">{post.title}</h1>
+        <figure>
+          <img
+            src={post.postDetails.img}
+            alt="Single Blog Post"
+            className="img-fluid mb-4"
+          />
+        </figure>
+        <h1 className="font-bold text-lg my-5">{post.postDetails.title}</h1>
 
-        <div className="mt-5">{post.short}</div>
+        <div className="my-5">{post.postDetails.content}</div>
       </section>
     </div>
   );
