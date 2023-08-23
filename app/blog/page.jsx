8fbf0dd569e544632361de73fcaf683c3f70fc87
Shakib <sarkeryasin614@gmail.com>
2023-08-23 23:37:49 +0600

    update

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -26,38 +26,3 @@ export default async function page() {
   );
 }
 
-// import getAllPosts from "@/lib/getAllPosts";
-// import Link from "next/link";
-
-// const page = async () => {
-//   const postBlog = await getAllPosts();
-//   return (
-//     <>
-//       <div className="container mx-auto my-2">
-//         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-//           {postBlog.length === 0 ? (
-//             <h2 className="text-red-500">Lodding......</h2>
-//           ) : (
-//             postBlog.map((posts, index) => (
-//               <Link
-//                 href={`/blog/${posts.id}`}
-//                 key={index.toString()}
-//                 className="card card-compact w-100 glass"
-//               >
-//                 <figure>
-//                   <img src={posts.img} alt="Post-image" />
-//                 </figure>
-//                 <div className="card-body">
-//                   <h2 className="card-title"> {posts.title} </h2>
-//                   <p> {posts.short} </p>
-//                 </div>
-//               </Link>
-//             ))
-//           )}
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default page;
